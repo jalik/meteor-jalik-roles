56a3fcbece0ba0a6ade3b30b6441c01058debd48
Karl STEIN <karl.stein.pro@gmail.com>
2015-10-16 12:31:07 -1000

    version 0.1.5 - added explicit dependencies - added LICENSE

--- package.js
+++ package.js
@@ -1,15 +1,18 @@
 Package.describe({
     name: 'jalik:roles',
-    version: '0.1.4',
+    version: '0.1.5',
     author: 'karl.stein.pro@gmail.com',
     summary: 'Easy but strong way to manage users permissions in your Meteor apps.',
     homepage: 'https://github.com/jalik/jalik-roles',
     git: 'https://github.com/jalik/jalik-roles.git',
-    documentation: 'README.md'
+    documentation: 'README.md',
+    license: 'MIT'
 });
 
 Package.onUse(function (api) {
     api.versionsFrom('1.1.0.2');
+    api.use('check');
+    api.use('underscore');
     api.use(['accounts-password', 'minimongo', 'mongo-livedata', 'templating', 'tracker'], 'client');
     api.use(['mongo'], 'server');
     api.addFiles('roles.js');
