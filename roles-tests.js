9224d4b47187a1ca6169b671d27f033004f9896f
jalik <karl.stein.pro@gmail.com>
2016-12-30 17:53:53 -1000

    Removes .versions file

--- roles-tests.js
+++ roles-tests.js
@@ -28,6 +28,7 @@ import {Meteor} from 'meteor/meteor';
 import {Roles} from 'meteor/jalik:roles';
 import {chai} from 'meteor/practicalmeteor:chai';
 
+
 describe('Roles', function () {
 
     let roleId;
